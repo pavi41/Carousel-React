85aa5a8432041da7d5b2b983b52b69a0e14fca44
unknown <pkhattry41@gmail.com>
2021-05-07 12:31:18 +0530

    reactjs project first commit

---
+++ src/components/SliderData.js
@@ -0,0 +1,17 @@
+export const SliderData = [
+    {
+      image: 'https://images.unsplash.com/photo-1538356913997-b04286765811?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI1fHhIeFlUTUhMZ09jfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'  
+    },
+    {
+      image: 'https://images.unsplash.com/photo-1620259570543-31964aa22586?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8eEh4WVRNSExnT2N8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'  
+    },
+    {
+      image: 'https://images.unsplash.com/photo-1620312584894-f134f1898d70?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDd8eEh4WVRNSExnT2N8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'  
+    },
+    {
+      image: 'https://images.unsplash.com/photo-1620189506492-c94da30667cb?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDEzfHhIeFlUTUhMZ09jfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'  
+    },
+    {
+      image: 'https://images.unsplash.com/photo-1620175527578-3a01876fd6c7?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE0fHhIeFlUTUhMZ09jfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'  
+    }
+]
